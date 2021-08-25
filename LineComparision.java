b3ecd2a55443a169ebd617645f2dc5544629b9fc
megha angadi <meghamangadi@gmail.com>
2021-08-25 21:30:00 +0530

    Added: UC2-to check equality of two lines

--- LineComparision.java
+++ LineComparision.java
@@ -7,29 +7,49 @@ public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparsion Computation Program");
 
-		int x1, x2, y1, y2;
-		double Distance;
-
+		int x1,x2,x3,x4,y1,y2,y3,y4 ;
+		double Distance1 ,Distance2 ;
+		
 		Scanner sc = new Scanner(System.in);
-
+	System.out.println("For line 1");
+		
 		System.out.println("Enter the value of X1");
 		x1 = sc.nextInt();
-
+		
 		System.out.println("Enter the value of X2");
 		x2 = sc.nextInt();
-
+		
 		System.out.println("Enter the value of Y1");
 		y1 = sc.nextInt();
-
+		
 		System.out.println("Enter the value of Y2");
 		y2 = sc.nextInt();
-		sc.close();
 		
-
-		Distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-
-		System.out.println("Length of a line : " + +Distance);
+    System.out.println("For line 2");
+		
+		System.out.println("Enter the value of X1");
+		x3 = sc.nextInt();
+		
+		System.out.println("Enter the value of X2");
+		x4 = sc.nextInt();
+		
+		System.out.println("Enter the value of Y1");
+		y3 = sc.nextInt();
+		
+		System.out.println("Enter the value of Y2");
+		y4 = sc.nextInt();
+		
+		Distance1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		Distance2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+		
+		System.out.println("Distance 1 :"+Distance1);
+		System.out.println("Distance 2 :"+Distance2);
+		
+		if(Distance1==Distance2) {
+			System.out.println("Length of two lines are equal");
+		}else {
+			System.out.println("Length of two lines are not equal");
+		}
 
 	}
-
 }
