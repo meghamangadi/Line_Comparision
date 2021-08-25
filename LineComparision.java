fb330f12fab6ac7014ab0c0cf72cc6ef2501cda3
megha angadi <meghamangadi@gmail.com>
2021-08-25 20:53:04 +0530

    Added:Welcome message for user

---
+++ LineComparision.java
@@ -0,0 +1,10 @@
+package bridgeLabz;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Line Comparsion Computation Program");
+
+	}
+
+}
