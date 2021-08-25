17deb92ef6a374c30d567cfaa7ae494cf665ceaa
megha angadi <meghamangadi@gmail.com>
2021-08-25 21:11:56 +0530

    Added:UC1-calculated the lenght of a line

--- LineComparision.java
+++ LineComparision.java
@@ -1,9 +1,34 @@
 package bridgeLabz;
 
+import java.util.Scanner;
+
 public class LineComparision {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparsion Computation Program");
+		System.out.println("Welcome to Line Comparsion Computation Program");
+
+		int x1, x2, y1, y2;
+		double Distance;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the value of X1");
+		x1 = sc.nextInt();
+
+		System.out.println("Enter the value of X2");
+		x2 = sc.nextInt();
+
+		System.out.println("Enter the value of Y1");
+		y1 = sc.nextInt();
+
+		System.out.println("Enter the value of Y2");
+		y2 = sc.nextInt();
+		sc.close();
+		
+
+		Distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("Length of a line : " + +Distance);
 
 	}
 
